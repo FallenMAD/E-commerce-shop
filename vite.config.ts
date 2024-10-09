9452c709afab0f90f11cd26a501c92c5a4c90561
FallenMAD <naruto.yzymaki4@gmail.com>
2024-10-09 14:58:35 +0200

    Third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 
 import { defineConfig } from 'vite';
-import { ghPages } from 'vite-plugin-gh-pages';
+// import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
+  base: '/E-commerce-shop',
 });
